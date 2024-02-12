4ae9ebde818b6c129c27a07db5372c8827dfba1c
Riamu360 <liamzuniga360@gmail.com>
2024-02-12 10:37:02 -0500

    Liam

--- src/EjercitoRuso/LZMecatronico.java
+++ src/EjercitoRuso/LZMecatronico.java
@@ -1,5 +1,55 @@
 package EjercitoRuso;
 
-public class Mecatronico {
+import java.util.Scanner;
+
+public class LZMecatronico extends LZHumano{
+
+    public LZMecatronico(String nombre) {
+        super(nombre);
+    }
+
+    Scanner sc = new Scanner(System.in);
+
+    public void ensamblarExobot(LESoldado leSoldado, ExoBot Exobot, IABot IaBot){
+        int opcionE;
+        System.out.println("El soldado consta de las siguientes habilidades: ");
+        System.out.println("Brazo Derecho, habilidad: " + leSoldado.getssHabilidadBrazoDerecho());
+        System.out.println("Brazo Izquierdo, habilidad: " + leSoldado.getssHabilidadBrazoIzquierdo());
+        System.out.println("Su ExoBot tendrá las siguientes armas: ");
+        Exobot.unirseSoldado(leSoldado);
+        
+        System.out.println("Emsamblando Exobot, por favor espere");
+        for (int i=0; i<=100; i++){
+            int nSpacios = 2-(i%3);
+            String bStatus = "";
+            for (int j = 0; j<nSpacios; j++){
+                bStatus += "o";
+            }
+            bStatus += "0";
+            for (int j = 0; j<2-nSpacios; j++){
+                bStatus += "o";
+            }
+            System.out.print(bStatus+" "+i+"%\r");
+            Thread.sleep(70);
+        }
+        System.out.println("Su ExoBot ha sido ensamblado");
+        do {
+            System.out.println("Desea ver su ExoBot: \n1. Si \n2. No");
+            opcionE = sc.nextInt();
+            switch (opcionE) {
+                case 1:
+                    LZDisenoExobot oLzDisenoExobot = new LZDisenoExobot();
+                    oLzDisenoExobot.lzVerExobot();
+                    opcionE = 2;
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("\nOpción no válida.\nPor favor ingrese una opción correcta.");
+                    break;
+            }
+        } while (opcionE != 2);
+
+    }
 
 }
